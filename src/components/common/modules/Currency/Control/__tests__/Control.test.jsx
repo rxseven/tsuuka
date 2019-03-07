12ae2036c6620739a794fcd0ea8d3363f3f623e1
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-07 20:41:26 +0700

    Remove conflict snapshot tests from Currency.Control

--- src/components/common/modules/Currency/Control/__tests__/Control.test.jsx
+++ src/components/common/modules/Currency/Control/__tests__/Control.test.jsx
@@ -1,6 +1,5 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import renderer from 'react-test-renderer';
 
 import Environment from 'tests/environment';
 import Control from '../index';
@@ -40,16 +39,6 @@ describe('<Currency.Control />', () => {
 
       // Assert
       expect(wrapper).toBeDefined();
-    });
-  });
-
-  describe('Snapshot tests', () => {
-    it('should render correctly', () => {
-      // Act
-      const tree = renderer.create(component).toJSON();
-
-      // Assert
-      expect(tree).toMatchSnapshot();
     });
   });
 });
