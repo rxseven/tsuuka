92e5f62475649b4c517278f0a5fb4a24ba2ccde2
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-07 19:23:24 +0700

    Refactor Brand component
    - Pass all props to a component
    - Change text color

--- src/components/common/base/Brand/index.jsx
+++ src/components/common/base/Brand/index.jsx
@@ -3,26 +3,22 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Logo = styled(Link)`
+  color: rgba(255, 255, 255, 0.75);
   display: block;
   font-size: 1.1rem;
   margin-bottom: 0;
   padding: 0;
   transition: color 0.2s ease-in-out;
 
-  &,
   :hover {
-    color: ${({ theme }) => theme.color.dark};
-  }
-
-  :hover {
-    color: ${({ theme }) => theme.color.primary};
+    color: rgba(255, 255, 255, 1);
     text-decoration: none;
   }
 `;
 
-function Brand() {
+function Brand(props) {
   return (
-    <Logo title="Currency Converter" to="/">
+    <Logo {...props} title="Currency Converter" to="/">
       通貨換算ツール
     </Logo>
   );
