e1a07faeab8731be78ba8fa51b9dbe538d93853c
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-07 22:17:28 +0700

    Fix inconsistent currency code by limiting string to 3 characters

--- src/components/common/modules/Currency/Dropdown/index.jsx
+++ src/components/common/modules/Currency/Dropdown/index.jsx
@@ -38,6 +38,7 @@ const propTypes = exact({
 function Dropdown({ data, query }) {
   const matches = match(data.name, query);
   const parts = parse(data.name, matches);
+  const currencyCode = data.code.substring(0, 3);
   const name = (
     <Name>
       {parts.map(part => {
@@ -55,7 +56,7 @@ function Dropdown({ data, query }) {
 
   return (
     <Item>
-      {name} <Code>{data.code}</Code>
+      {name} <Code>{currencyCode}</Code>
     </Item>
   );
 }
