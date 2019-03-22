324807a6add74037578d0bbb17b8b3ddede5f699
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 16:21:33 +0700

    Uninstall enzyme package & configuration

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,7 +1,2 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
-
-// Configure Enzyme adapter
-configure({ adapter: new Adapter() });
