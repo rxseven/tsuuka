37079158f277e7966cf91229b666b8615116679f
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 16:15:19 +0700

    Refactor & add more unit tests for covering Providers component

---
+++ src/components/core/skeleton/Providers/__tests__/Providers.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { factory, toMarkup } from 'tests/utilities';
+import Providers from '../index';
+
+// Arrange
+const seed = { content: 'content' };
+const source = { children: <span>{seed.content}</span> };
+const input = { ...seed, ...source };
+
+// Setup
+function setup(props) {
+  return factory(Providers, source, props);
+}
+
+// Test suites
+describe('<Providers />', () => {
+  it('should render without crashing', () => {
+    setup();
+  });
+
+  it('should render passed children correctly', () => {
+    const expected = {
+      content: input.content,
+      html: input.children
+    };
+    const { component } = setup();
+
+    expect(component).toHaveTextContent(expected.content);
+    expect(component).toContainHTML(toMarkup(expected.html));
+  });
+});
