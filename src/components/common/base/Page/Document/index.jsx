bf69e4e45b32ceba51dab32969532797a4ba49e3
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 15:54:02 +0700

    Add children wrapper to Page.Document component

--- src/components/common/base/Page/Document/index.jsx
+++ src/components/common/base/Page/Document/index.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
+import React from 'react';
 
 const propTypes = exact({
   children: PropTypes.node.isRequired
 });
 
 function Document({ children }) {
-  return children;
+  return <div>{children}</div>;
 }
 
 Document.propTypes = propTypes;
