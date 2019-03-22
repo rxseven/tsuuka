4f84becd46c1cfde70257ace09f2c1a7400af125
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 16:11:18 +0700

    Refactor & add more unit tests for covering Currency.Control component

--- src/components/common/modules/Currency/Control/__tests__/Control.test.jsx
+++ src/components/common/modules/Currency/Control/__tests__/Control.test.jsx
@@ -1,44 +1,38 @@
-import { shallow } from 'enzyme';
 import React from 'react';
 
-import Environment from 'tests/environment';
+import { factory } from 'tests/utilities';
 import Control from '../index';
 
+// Arrange
+const source = {
+  baseAmount: 100,
+  baseCurrency: { code: 'USD', name: 'United States Dollar' },
+  baseInputRef: React.createRef(),
+  currencies: [
+    { code: 'EUR', name: 'Euro' },
+    { code: 'THB', name: 'Thai Baht' },
+    { code: 'USD', name: 'United States Dollar' }
+  ],
+  isProcessing: false,
+  onAmount: jest.fn(),
+  onExchange: jest.fn(),
+  onReset: jest.fn(),
+  onSelect: jest.fn(),
+  override: false,
+  rate: '31.7916',
+  targetAmount: '87.892',
+  targetCurrency: { code: 'THB', name: 'Thai Baht' },
+  targetInputRef: React.createRef()
+};
+
+// Setup
+function setup(props) {
+  return factory(Control, source, props);
+}
+
+// Test suites
 describe('<Currency.Control />', () => {
-  // Arrange
-  const props = {
-    baseAmount: 100,
-    baseCurrency: { code: 'USD', name: 'United States Dollar' },
-    baseInputRef: React.createRef(),
-    currencies: [
-      { code: 'EUR', name: 'Euro' },
-      { code: 'THB', name: 'Thai Baht' },
-      { code: 'USD', name: 'United States Dollar' }
-    ],
-    isProcessing: false,
-    onAmount: jest.fn(),
-    onExchange: jest.fn(),
-    onReset: jest.fn(),
-    onSelect: jest.fn(),
-    override: false,
-    rate: '31.7916',
-    targetAmount: '87.892',
-    targetCurrency: { code: 'THB', name: 'Thai Baht' },
-    targetInputRef: React.createRef()
-  };
-  const component = (
-    <Environment>
-      <Control {...props} />
-    </Environment>
-  );
-
-  describe('Unit tests', () => {
-    it('should render without crashing', () => {
-      // Act
-      const wrapper = shallow(component);
-
-      // Assert
-      expect(wrapper).toBeDefined();
-    });
+  it('should render without crashing', () => {
+    setup();
   });
 });
