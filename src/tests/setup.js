ce1e954be6de9aa59e9ee01e54591f24ad65441f
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 16:20:02 +0700

    Uninstall jest-enzyme package & configuration

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,7 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-dom/extend-expect';
-import 'jest-enzyme';
 import 'react-testing-library/cleanup-after-each';
 
 // Configure Enzyme adapter
