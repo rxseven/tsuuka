55d085189a27d1ce5f493a2e96980056d0a17d2f
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 16:11:30 +0700

    Refactor & add more unit tests for covering Currency.Dropdown component

--- src/components/common/modules/Currency/Dropdown/__tests__/Dropdown.test.jsx
+++ src/components/common/modules/Currency/Dropdown/__tests__/Dropdown.test.jsx
@@ -1,39 +1,20 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import Environment from 'tests/environment';
+import { factory } from 'tests/utilities';
 import Dropdown from '../index';
 
+// Arrange
+const source = {
+  data: { code: 'USD', name: 'United States Dollar' },
+  query: 'THB'
+};
+
+// Setup
+function setup(props) {
+  return factory(Dropdown, source, props);
+}
+
+// Test suites
 describe('<Currency.Dropdown />', () => {
-  // Arrange
-  const props = {
-    data: { code: 'USD', name: 'United States Dollar' },
-    query: 'THB'
-  };
-  const component = (
-    <Environment>
-      <Dropdown {...props} />
-    </Environment>
-  );
-
-  describe('Unit tests', () => {
-    it('should render without crashing', () => {
-      // Act
-      const wrapper = shallow(component);
-
-      // Assert
-      expect(wrapper).toBeDefined();
-    });
-  });
-
-  describe('Snapshot tests', () => {
-    it('should render correctly', () => {
-      // Act
-      const tree = renderer.create(component).toJSON();
-
-      // Assert
-      expect(tree).toMatchSnapshot();
-    });
+  it('should render without crashing', () => {
+    setup();
   });
 });
