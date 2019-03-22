6ed18fba75c0daa716038299b0e85ae58a3fcebf
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 15:52:38 +0700

    Add missing type checking for Page.Body component

--- src/components/common/base/Page/Body/index.jsx
+++ src/components/common/base/Page/Body/index.jsx
@@ -1,5 +1,14 @@
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
+
+const propTypes = exact({
+  children: PropTypes.node.isRequired
+});
+
 function Body({ children }) {
   return children;
 }
+
+Body.propTypes = propTypes;
 
 export default Body;
