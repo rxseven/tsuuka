6e3b2523681d2e86ebf2aebf44586753842ae160
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 15:54:18 +0700

    Add missing type checking for Page.Head component

--- src/components/common/base/Page/Head/index.jsx
+++ src/components/common/base/Page/Head/index.jsx
@@ -1,8 +1,16 @@
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+
+const propTypes = exact({
+  children: PropTypes.node.isRequired
+});
 
 function Head({ children }) {
   return <Helmet>{children}</Helmet>;
 }
+
+Head.propTypes = propTypes;
 
 export default Head;
