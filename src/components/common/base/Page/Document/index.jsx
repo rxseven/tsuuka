41472b54890e9e151758edb2bbc96a4d6e194f12
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-22 15:53:44 +0700

    Add missing type checking for Page.Document component

--- src/components/common/base/Page/Document/index.jsx
+++ src/components/common/base/Page/Document/index.jsx
@@ -1,5 +1,14 @@
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
+
+const propTypes = exact({
+  children: PropTypes.node.isRequired
+});
+
 function Document({ children }) {
   return children;
 }
+
+Document.propTypes = propTypes;
 
 export default Document;
