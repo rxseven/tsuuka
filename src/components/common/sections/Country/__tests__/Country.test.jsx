1af0e34b06c8ad9978ab81c6aba74c02525b5cd8
ME (Mac mini) <rxseven.com@gmail.com>
2019-04-02 14:34:25 +0700

    Add more unit tests for covering Currency.Country component

--- src/components/common/sections/Country/__tests__/Country.test.jsx
+++ src/components/common/sections/Country/__tests__/Country.test.jsx
@@ -2,6 +2,7 @@ import { factory } from 'tests/utilities';
 import Country from '../index';
 
 // Arrange
+const API_URL = 'https://www.countryflags.io';
 const source = {
   data: [
     { code: 'ES', name: 'Spain' },
@@ -19,5 +20,22 @@ function setup(props) {
 describe('<Country />', () => {
   it('should render without crashing', () => {
     setup();
+  });
+
+  it('should render country name', () => {
+    const expected = { content: source.data[0].name };
+    const { component } = setup();
+
+    expect(component).toHaveTextContent(expected.content);
+  });
+
+  it('should render country flag', () => {
+    const expected = { url: `${API_URL}/${source.data[2].code}/flat/32.png` };
+    const { getByAltText } = setup();
+
+    expect(getByAltText(source.data[2].name)).toHaveAttribute(
+      'src',
+      expected.url
+    );
   });
 });
