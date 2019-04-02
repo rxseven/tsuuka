017e1eef45969956a4e31279a949ce564f9f2a3d
ME (Mac mini) <rxseven.com@gmail.com>
2019-04-02 14:29:38 +0700

    Add more unit tests for covering Currency.Dropdown component

--- src/components/common/modules/Currency/Dropdown/__tests__/Dropdown.test.jsx
+++ src/components/common/modules/Currency/Dropdown/__tests__/Dropdown.test.jsx
@@ -4,7 +4,7 @@ import Dropdown from '../index';
 // Arrange
 const source = {
   data: { code: 'USD', name: 'United States Dollar' },
-  query: 'THB'
+  query: 'USD'
 };
 
 // Setup
@@ -16,5 +16,21 @@ function setup(props) {
 describe('<Currency.Dropdown />', () => {
   it('should render without crashing', () => {
     setup();
+  });
+
+  it('should render text content (currency code and name)', () => {
+    const expected = { ...source.data };
+    const { component } = setup();
+
+    expect(component).toHaveTextContent(expected.code);
+    expect(component).toHaveTextContent(expected.name);
+  });
+
+  it('should render currency code with only 3 characters)', () => {
+    const props = { ...source.data, code: 'USDx' };
+    const expected = { code: source.data.code };
+    const { component } = setup(props);
+
+    expect(component).toHaveTextContent(expected.code);
   });
 });
